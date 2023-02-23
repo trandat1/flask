dbc610b17f6586210968d69d7467d3d8cd8fd406
trandat1 <GiaVien2002@gmail.com>
2023-02-23 14:58:03 +0700

    add flask

---
+++ market/routes.py
@@ -0,0 +1,79 @@
+from market import app
+from flask import render_template, redirect, url_for, flash,request
+from market.models import Item, User
+from market.forms import RegisterForm, LoginForm,PurchaseItemForm
+from market import db
+from flask_login import login_user,logout_user,login_required,current_user
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market',methods=['GET', 'POST'])
+@login_required
+def market_page():
+    purchase_form=PurchaseItemForm()
+    if request.method == "POST":
+        purchased_item=request.form.get('purchased_item')
+        p_item_object=Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            p_item_object.owner=current_user.id
+            current_user.budget-=p_item_object.price
+            db.session.commit()
+    items = Item.query.filter_by(owner=None)
+
+    return render_template('market.html', items=items,purchase_form=purchase_form)
+
+
+@app.route('/flask')
+def flask_page():
+    return render_template('flask.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully !You are logged in as:{user_to_create.username}',category='success')
+
+
+        return redirect(url_for('home_page'))
+    if form.errors != {}:  # if there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(
+                f'There Was an erro with creating a user:{err_msg}', category='danger')
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success !You are logged in as:{attempted_user.username}',category='success')
+            return redirect(url_for('market_page'))
+
+        else:
+            flash('Username and password are not match!Plesase try again',category='danger')
+
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("you have been logout!",category='info')
+    return redirect(url_for("home_page"))
+
---
+++ run.py
@@ -0,0 +1,5 @@
+from market import app
+
+#check if the run.py
+if __name__ == '__main__':
+    app.run(debug=True)
